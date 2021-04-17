f0bd7dc9534b7a3758d9594558a61d5b9565c6af
K. Lange <klange@toaruos.org>
2021-04-17 21:44:10 +0900

    More docs cleanup

--- docs/menudata.js
+++ docs/menudata.js
@@ -22,12 +22,6 @@ for the JavaScript code in this file
 */
 var menudata={children:[
 {text:"Home",url:"index.html"},
-{text:"Language",url:"lang_intro.html",children:[
-{text:"Language",url:"lang_intro.html"},
-{text:"Compilation",url:"lang_compiler.html"},
-{text:"Objects",url:"lang_objects.html"},
-{text:"Expressions",url:"lang_expressions.html"},
-{text:"Statements",url:"lang_statements.html"}]},
 {text:"Modules",url:"modulelist.html",children:[
 {text:"Module List",url:"modulelist.html"},
 {text:"Class Index",url:"classindex.html"},
